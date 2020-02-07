5c8960c35abd497669049beeab25c57091148145
candescent-mouse-0917 <aqiph818@gmail.com>
2020-02-07 05:01:20 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,24 @@
+//forLoop
+function forLoop(array){
+  for (let i=0;i<25;i++){
+    if (i===1){
+      array.push('I am 1 strange loop.')
+    }
+    else{
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array
+}
+
+//whileLoop
+function whileLoop(num){
+  while (num > 0){
+    console.log(num);
+    --num;
+  }
+  return 'done';
+}
+
+
+
